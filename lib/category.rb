f287b129fa71cc6f0d6ea13a3412bea98d278da3
Christian <ckdanielsen@gmail.com>
2014-08-22 11:04:38 -0700

    add create/read methods to category class

--- lib/category.rb
+++ lib/category.rb
@@ -4,6 +4,26 @@ attr_reader :slant
 
   def initialize attributes
     @slant = attributes[:slant]
+    @id = nil
+  end
+
+  def self.all
+    results = DB.exec('SELECT * FROM categorys;')
+    categorys = []
+    results.each do |result|
+      slant = result['slant']
+      categorys << Category.new({slant: slant})
+    end
+    categorys
+  end
+
+  def save
+    result = DB.exec("INSERT INTO categorys (slant) VALUES ('#{@slant}') RETURNING id;")
+    @id = result.first['id'].to_i
+  end
+
+  def ==(another_category)
+    self.slant == another_category.slant
   end
 
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -4,10 +4,35 @@ describe Category do
 
   describe 'initialize' do
     it 'is initialized with a hash of attributes' do
-      test_category = Category.new({slant: 'leftie'})
-      expect(test_category).to be_a Category
+      test_category1 = Category.new({slant: 'leftie'})
+      expect(test_category1).to be_a Category
     end
   end
 
+  describe ".all" do
+    it 'shows all categories currently saved in the database' do
+      test_category1 = Category.new({slant: 'leftie'})
+      test_category2 = Category.new({slant: 'right-of-center'})
+      test_category1.save
+      test_category2.save
+      expect(Category.all).to eq [test_category1, test_category2]
+    end
+  end
+
+  describe "#save" do
+    it 'lets you save a category to the database' do
+      test_category1 = Category.new({slant: 'leftie'})
+      test_category1.save
+      expect(Category.all).to eq [test_category1]
+    end
+  end
+
+  describe '==' do
+    it 'is the same category if it has the same slant' do
+      test_category1 = Category.new({slant: 'leftie'})
+      test_category2 = Category.new({slant: 'leftie'})
+      expect(test_category1).to eq test_category2
+    end
+  end
 
 end
